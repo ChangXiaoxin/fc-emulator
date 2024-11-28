7c142e3c922162111441cf09ab88ca56004869af
ChangXiaoxin <changx1922330593@gmail.com>
2024-11-28 18:46:59 +0800

    Add ppu regs for CPU bus.

--- src/FC/CPUBus.ts
+++ src/FC/CPUBus.ts
@@ -18,6 +18,34 @@ export class CPUBus implements IBus {
     else if (address < 0x4000){
       // PPU regs
       address = address & 0x0007;
+      switch (address){
+        case 0x0000:
+          this.ppu.regs[PPUReg.CTRL] = data;
+          break;
+        case 0x0001:
+          this.ppu.regs[PPUReg.MASK] = data;
+          break;
+        case 0x0002:
+          // Can't write PPU status.
+          break;
+        case 0x0003:
+          this.ppu.regs[PPUReg.OAMADDR] = data;
+          break;
+        case 0x0004:
+          this.ppu.regs[PPUReg.OAMDATA] = data;
+          break;
+        case 0x0005:
+          this.ppu.regs[PPUReg.SCROLL] = data;
+          break;
+        case 0x0006:
+          this.ppu.regs[PPUReg.ADDR] = data;
+          break;
+        case 0x0007:
+          this.ppu.regs[PPUReg.DATA] = data;
+          break;
+        default:
+          break;
+      }
       this.ppu.regs[address] = data;
     }
     else if (address < 0x6000){
@@ -33,27 +61,56 @@ export class CPUBus implements IBus {
     }
   }
   public readByte(address: uint16): uint8 {
+    let memory = 0xFF;
     if (address < 0x2000){
       // RAM
-      return this.ram[address & 0x07FF];
+      memory = this.ram[address & 0x07FF];
     }
     else if (address < 0x4000){
       // PPU regs
       address = address & 0x0007;
-      return this.ppu.regs[address];
+      switch (address){
+        case 0x0000:
+          memory = this.ppu.regs[PPUReg.CTRL];
+          break;
+        case 0x0001:
+          memory = this.ppu.regs[PPUReg.MASK];
+          break;
+        case 0x0002:
+          memory = this.ppu.regs[PPUReg.STATUS];
+          break;
+        case 0x0003:
+          memory = this.ppu.regs[PPUReg.OAMADDR];
+          break;
+        case 0x0004:
+          memory = this.ppu.regs[PPUReg.OAMDATA];
+          break;
+        case 0x0005:
+          memory = this.ppu.regs[PPUReg.SCROLL];
+          break;
+        case 0x0006:
+          memory = this.ppu.regs[PPUReg.ADDR];
+          break;
+        case 0x0007:
+          memory = this.ppu.regs[PPUReg.DATA];
+          break;
+        default:
+          break;
+      }
     }
     else if (address < 0x6000){
       // PPU OAMDMA
       if (address === 0x4014){
-        return this.ppu.regs[PPUReg.OAMDMA];
+        memory = this.ppu.regs[PPUReg.OAMDMA];
       }
       // IO Registers
-      return 0xFF;
+
     }
     else{
       // Cartridge
-      return this.cartridge.mapper.read(address);
+      memory = this.cartridge.mapper.read(address);
     }
+    return memory & 0xFF;
   }
   public writeWord(address: uint16, data: uint16): void {
     this.writeByte(address, data & 0xFF);

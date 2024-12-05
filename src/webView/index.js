f74c04dd7de3ff113d3fed35e913af7fe23bf245
ChangXiaoxin <changx1922330593@gmail.com>
2024-12-05 22:22:51 +0800

    Trick in CRT

--- src/webView/index.js
+++ src/webView/index.js
@@ -154,14 +154,11 @@ function drawImageWithTileCRT(image_w, image_h, tile_w, tile_h, image_in){
           // if (h === 0 && (w === tile_w-1)){
           if (h === tile_h-1){
             // // Draw the pixel darker on the right bottom of the tile.
+            let sum = image_in[(i*image_w+j)*4 + 0] + image_in[(i*image_w+j)*4 + 1] + image_in[(i*image_w+j)*4 + 2];
             image.data[((i*image_w*tile_h + j + h*image_w)*tile_w + w)*4 + 0] = image_in[(i*image_w+j)*4 + 0];
             image.data[((i*image_w*tile_h + j + h*image_w)*tile_w + w)*4 + 1] = image_in[(i*image_w+j)*4 + 1];
             image.data[((i*image_w*tile_h + j + h*image_w)*tile_w + w)*4 + 2] = image_in[(i*image_w+j)*4 + 2];
-            image.data[((i*image_w*tile_h + j + h*image_w)*tile_w + w)*4 + 3] = 0x8F;
-            // image.data[((i*image_w*tile_h + j + h*image_w)*tile_w + w)*4 + 0] = 0x00;
-            // image.data[((i*image_w*tile_h + j + h*image_w)*tile_w + w)*4 + 1] = 0x00;
-            // image.data[((i*image_w*tile_h + j + h*image_w)*tile_w + w)*4 + 2] = 0x00;
-            // image.data[((i*image_w*tile_h + j + h*image_w)*tile_w + w)*4 + 3] = 0xFF;
+            image.data[((i*image_w*tile_h + j + h*image_w)*tile_w + w)*4 + 3] = (sum === 0) ? 0xFF : 0x4F;
           }
           else{
             image.data[((i*image_w*tile_h + j + h*image_w)*tile_w + w)*4 + 0] = image_in[(i*image_w+j)*4 + 0];
